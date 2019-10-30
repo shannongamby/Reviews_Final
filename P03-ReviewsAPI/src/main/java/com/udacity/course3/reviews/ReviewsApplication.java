beb09e25bc7cadfa93a5e2ad8bd228b74d36f838
Shannon Gamby <shannongamby@hotmail.com>
2019-10-30 12:00:34 +0000

    wired up mongo db

--- P03-ReviewsAPI/src/main/java/com/udacity/course3/reviews/ReviewsApplication.java
+++ P03-ReviewsAPI/src/main/java/com/udacity/course3/reviews/ReviewsApplication.java
@@ -1,10 +1,25 @@
 package com.udacity.course3.reviews;
 
+import com.mongodb.MongoClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableMongoRepositories
 public class ReviewsApplication {
+
+	@Bean
+	public MongoClient mongo() {
+		return new MongoClient("localhost");
+	}
+
+	@Bean
+	public MongoTemplate mongoTemplate() throws Exception {
+		return new MongoTemplate(mongo(), "reviews");
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ReviewsApplication.class, args);
